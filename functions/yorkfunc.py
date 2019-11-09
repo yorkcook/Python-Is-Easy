729da00b34bb778de5183e4227f7996b567c43a0
York Cook <yorkcook@Johnny-Castle.lan1>
2019-11-09 17:35:43 -0500

    WIP: first two lessons completed

---
+++ functions/yorkfunc.py
@@ -0,0 +1,19 @@
+def singer():
+    best_singer = 'Frank Sinatra'
+    return best_singer
+
+def song_name():
+    hit_song = 'Come Fly With Me'
+    return hit_song
+
+def song_summary():
+    summary = 'Frank Sinatra released the hit Come Fly With Me in 1958. You might also recongized it from the movie Catch Me If You Can starring Leonardo DiCaprio!'
+    return summary
+
+
+print('Singer Name:', singer())
+
+print('Song Name:', song_name())
+
+print('Song Description:', song_summary())
+
---
+++ variables/yorkvar.py
@@ -0,0 +1,29 @@
+#This file is the first HW assignment for Python Is Easy by Pirple
+
+artist = 'Frank Sinatra'
+
+genre = 'Vocal Jazz'
+
+song_title = 'Come Fly With Me'
+
+year_released = 1958
+
+song_duration_seconds = 198
+
+song_duration_minutes_and_seconds = 3.18
+
+song_summary = 'Frank Sinatra released the hit Come Fly With Me in 1958. You might recognize it from the movie Catch Me If You Can starring Leonardo DiCaprio!'
+
+print(artist)
+print(genre)
+print(song_title)
+print(year_released)
+print(song_duration_seconds)
+print(song_duration_minutes_and_seconds)
+print(song_summary)
+
+
+'''
+Frank Sinatra is from New Jersey and was a top artist for many decades.
+I consider him to be one of, if not my favorite artist to listen to.
+'''
